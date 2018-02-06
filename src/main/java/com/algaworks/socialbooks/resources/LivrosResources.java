46f049e6f19feb6eb8ad4f1a4d15db6156ba946e
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2018-02-06 14:25:54 -0200

    Implemented the method post.

--- src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -3,6 +3,7 @@ package com.algaworks.socialbooks.resources;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,25 +12,20 @@ import com.algaworks.socialbooks.domain.Livro;
 import com.algaworks.socialbooks.repository.LivrosRepository;
 
 @RestController
+@RequestMapping("/livros")
 public class LivrosResources {
 
 	@Autowired
 	private LivrosRepository livrosRepository;
 
-	@RequestMapping(value = "/livros", method = RequestMethod.GET)
+	@RequestMapping(method = RequestMethod.GET)
 	public List<Livro> listar() {
-		
+
 		return livrosRepository.findAll();
-		
-		/*
-		 * Livro l1 = new Livro("Rest Aplicado"); Livro l2 = new
-		 * Livro("Git passo-a-passo");
-		 * 
-		 * Livro[] livros = { l1, l2 };
-		 * 
-		 * return Arrays.asList(livros);
-		 */
-		
-		
+	}
+
+	@RequestMapping(method = RequestMethod.POST)
+	public void salvar(@RequestBody Livro livro) {
+		livrosRepository.save(livro);
 	}
 }
