1740e88f2bf61590787fee209a8847f66a8689de
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2018-02-23 16:39:40 -0300

    Formatting Json Formats

--- src/main/java/com/algaworks/socialbooks/domain/Comentario.java
+++ src/main/java/com/algaworks/socialbooks/domain/Comentario.java
@@ -10,7 +10,10 @@ import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.annotation.JsonInclude.Include;
 
 @Entity
 public class Comentario {
@@ -18,9 +21,15 @@ public class Comentario {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
+	
 	private String texto;
+	
+	@JsonInclude(Include.NON_NULL)
 	private String usuario;
+	
+	@JsonFormat(pattern = "dd/MM/yyyy")
 	private Date data;
+	
 	@ManyToOne(fetch = FetchType.LAZY)
 	@JoinColumn(name = "LIVRO_ID")
 	@JsonIgnore
