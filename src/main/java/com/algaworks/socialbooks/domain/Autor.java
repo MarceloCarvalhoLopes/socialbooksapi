94b77d9e9e7fb0bd559ae7d4738529ca6517bc0a
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2018-02-25 17:33:49 -0300

    Validating data entry

--- src/main/java/com/algaworks/socialbooks/domain/Autor.java
+++ src/main/java/com/algaworks/socialbooks/domain/Autor.java
@@ -8,6 +8,9 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.validator.constraints.NotEmpty;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnore;
@@ -20,53 +23,64 @@ public class Autor {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-	
+
+	@NotEmpty(message = "O campo nome não pode ser vazio.")
 	private String nome;
-	
+
 	@JsonFormat(pattern = "dd/MM/yyyy")
 	@JsonInclude(Include.NON_NULL)
+	@NotNull(message = "O campo nascimento é de preenchimento obrigatório")
 	private Date nascimento;
-	
+
 	@JsonInclude(Include.NON_NULL)
+	@NotNull(message = "O campo nacionalidade é de preenchimento obrigatório")
 	private String nacionalizacao;
 
 	@OneToMany(mappedBy = "autor")
 	@JsonIgnore
 	private List<Livro> livro;
-	
+
 	public Autor() {
 	}
-	
+
 	public Long getId() {
 		return id;
 	}
+
 	public void setId(Long id) {
 		this.id = id;
 	}
+
 	public String getNome() {
 		return nome;
 	}
+
 	public void setNome(String nome) {
 		this.nome = nome;
 	}
+
 	public Date getNascimento() {
 		return nascimento;
 	}
+
 	public void setNascimento(Date nascimento) {
 		this.nascimento = nascimento;
 	}
+
 	public String getNacionalizacao() {
 		return nacionalizacao;
 	}
+
 	public void setNacionalizacao(String nacionalizacao) {
 		this.nacionalizacao = nacionalizacao;
 	}
+
 	public List<Livro> getLivro() {
 		return livro;
 	}
+
 	public void setLivro(List<Livro> livro) {
 		this.livro = livro;
 	}
-	
-	
+
 }
