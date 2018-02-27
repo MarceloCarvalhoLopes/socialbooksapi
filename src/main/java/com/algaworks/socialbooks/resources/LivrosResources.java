78bae9c065a2d804e7f7a2e8b2bc4576cb2fb69d
MarceloCarvalhoLopes <marcelolcarvalho@gmail.com>
2018-02-27 13:25:49 -0300

    Autentication with Spring Security

--- src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
+++ src/main/java/com/algaworks/socialbooks/resources/LivrosResources.java
@@ -2,12 +2,16 @@ package com.algaworks.socialbooks.resources;
 
 import java.net.URI;
 import java.util.List;
+import java.util.concurrent.TimeUnit;
 
 import javax.validation.Valid;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.CacheControl;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -46,7 +50,9 @@ public class LivrosResources {
 
 		Livro livro = livrosService.buscar(id);
 
-		return ResponseEntity.status(HttpStatus.OK).body(livro);
+		CacheControl cacheControl = CacheControl.maxAge(20, TimeUnit.SECONDS);
+
+		return ResponseEntity.status(HttpStatus.OK).cacheControl(cacheControl).body(livro);
 
 	}
 
@@ -71,6 +77,10 @@ public class LivrosResources {
 	public ResponseEntity<Void> adicionarComentario(@PathVariable("id") Long livroId,
 			@RequestBody Comentario comentario) {
 
+		Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+		
+		comentario.setUsuario(auth.getName());
+		
 		livrosService.salvarComentario(livroId, comentario);
 
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().build().toUri();
@@ -81,10 +91,10 @@ public class LivrosResources {
 	@RequestMapping(value = "/{id}/comentarios", method = RequestMethod.GET)
 	public ResponseEntity<List<Comentario>> listarComentarios(@PathVariable("id") Long livroId) {
 
-		List<Comentario>comentarios = livrosService.listarComentarios(livroId);
-		
+		List<Comentario> comentarios = livrosService.listarComentarios(livroId);
+
 		return ResponseEntity.status(HttpStatus.OK).body(comentarios);
-		
+
 	}
 
 }
